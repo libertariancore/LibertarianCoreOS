e99193b91e8f8bd4411321a912740a232807efea
libertariancore <libertarianus@protonmail.com>
2026-05-04 00:41:48 +0200

    Update post-install.sh

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -oue pipefail
+set -eu
 
 # 1. Hardening Réseau (Anti-IPv6)
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
@@ -13,12 +13,8 @@ dns=none
 EOF
 echo "nameserver 9.9.9.9" > /etc/resolv.conf
 
-# 3. Activation services (symlinks uniquement, pas de démarrage)
-systemctl enable libvirtd
-systemctl enable waydroid-container
+# 3. Activation services
+systemctl enable libvirtd || true
 
-# 4. Fish comme shell par défaut système
-sed -i 's|/bin/bash|/usr/bin/fish|g' /etc/default/useradd || true
-
-# 5. Sécurité Hardware (Blacklist Bluetooth)
+# 4. Sécurité Hardware (Blacklist Bluetooth)
 echo "blacklist btusb" > /etc/modprobe.d/disable-bluetooth.conf
