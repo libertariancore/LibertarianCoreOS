6722eb5d8454a49881297ce161c4a575db804e34
libertariancore <libertarianus@protonmail.com>
2026-05-04 00:20:00 +0200

    Update post-install.sh

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
+set -oue pipefail
 
 # 1. Shell Fish par défaut
-chsh -s /usr/bin/fish
+chsh -s /usr/bin/fish || true
 
 # 2. Hardening Réseau (Anti-Tracking & Anti-IPv6)
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
@@ -17,40 +17,12 @@ EOF
 echo "nameserver 9.9.9.9" > /etc/resolv.conf
 
 # 4. Activation des services (Virtualisation + IA + Android)
-systemctl enable libvirtd
-systemctl enable --now waydroid-container
+systemctl enable libvirtd || true
+systemctl enable --now waydroid-container || true
 
 # 5. IA Locale (Ollama)
 curl -fsSL https://ollama.com/install.sh | sh
-systemctl enable ollama
+systemctl enable ollama || true
 
 # 6. Sécurité Hardware (Blacklist Bluetooth)
 echo "blacklist btusb" > /etc/modprobe.d/disable-bluetooth.conf
-
-flatpak remote-add --if-not-exists flathub \
-  https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak install -y --noninteractive flathub \
-  com.brave.Browser \
-  io.gitlab.librewolf-community.librewolf \
-  org.torproject.torbrowser-launcher \
-  io.simplex.simplex \
-  com.github.tchx84.Flatseal \
-  com.belmoussaoui.Authenticator \
-  org.onlyoffice.desktopeditors \
-  net.ankiweb.Anki \
-  org.gnome.Thunderbird \
-  com.visualstudio.code \
-  com.usebottles.bottles \
-  com.heroicgameslauncher.hgl \
-  com.github.Matoking.protontricks \
-  net.lutris.Lutris \
-  tv.kodi.Kodi \
-  org.videolan.VLC \
-  io.bassi.Amberol \
-  io.github.ferraridamiano.ConverterNOW \
-  io.gitlab.news_flash.NewsFlash \
-  net.cozic.joplin_desktop \
-  org.localsend.localsend_app \
-  com.ktechpit.ultimate-media-downloader \
-  com.mullvad.MullvadVPN
