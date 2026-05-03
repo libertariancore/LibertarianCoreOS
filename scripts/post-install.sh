981be84e8c1f8c8317bccdd973605905894085ca
libertariancore <libertarianus@protonmail.com>
2026-05-04 00:47:59 +0200

    Update post-install.sh

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-set -eu
 
-# 1. Hardening Réseau (Anti-IPv6)
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+# 1. Anti-IPv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf || true
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf || true
 
-# 2. DNS Souverain (Quad9)
-mkdir -p /etc/NetworkManager/conf.d
-cat <<EOF > /etc/NetworkManager/conf.d/dns.conf
-[main]
-dns=none
-EOF
-echo "nameserver 9.9.9.9" > /etc/resolv.conf
-
-# 3. Activation services
-systemctl enable libvirtd || true
-
-# 4. Sécurité Hardware (Blacklist Bluetooth)
-echo "blacklist btusb" > /etc/modprobe.d/disable-bluetooth.conf
+# 2. Blacklist Bluetooth
+echo "blacklist btusb" > /etc/modprobe.d/disable-bluetooth.conf || true
