d0f06d278a766df924fb1a4b9dc93ca6b20a8fb8
libertariancore <libertarianus@protonmail.com>
2026-05-04 01:13:55 +0200

    Update post-install.sh

--- files/scripts/post-install.sh
+++ files/scripts/post-install.sh
@@ -5,7 +5,7 @@ set -eu
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 
-# 2. DNS Quad9
+# 2. DNS Quad9 via NetworkManager
 mkdir -p /etc/NetworkManager/conf.d
 cat <<EOF > /etc/NetworkManager/conf.d/dns.conf
 [main]
@@ -15,5 +15,8 @@ EOF
 # 3. Services
 systemctl enable libvirtd || true
 
-# 4. Blacklist Bluetooth
+# 4. Fish comme shell par défaut système
+sed -i 's|/bin/bash|/usr/bin/fish|g' /etc/default/useradd || true
+
+# 5. Blacklist Bluetooth
 echo "blacklist btusb" > /etc/modprobe.d/disable-bluetooth.conf
